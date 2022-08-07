c827d0519e20f3f9c4a7077d2b664261ee2f30d3
Michał Łaszczewski <michal@laszczewski.com>
2022-08-07 18:06:15 +0200

    access method fix

--- framework/lib/processors/accessMethod.js
+++ framework/lib/processors/accessMethod.js
@@ -2,7 +2,8 @@ function getAccessMethod(access) {
   if(typeof access == 'function') {
     return access
   } else if(Array.isArray(access)) {
-    return (params, {service, client}) => {
+    return (params, { service, client }) => {
+      if(client.roles.includes('administrator')) return true
       if(client.roles.includes('admin')) return true
       for(let role of access) if(client.roles.includes(role)) return true
       return false
