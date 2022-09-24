f94e5dde7de8dc425ae776a017c5e8fb2c2cea2d
Michał Łaszczewski <michal@laszczewski.com>
2022-09-24 15:11:21 +0200

    multiple architectures support

--- db-server/build-docker-and-upload-aarch64.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-VERSION=`echo "console.log(require('./package.json').version)" | node`
-echo building docker image for version $VERSION
-docker build -t livechange/db-server-aarch64:$VERSION -t livechange/db-server-aarch64:latest .
-echo uploading docker image to docker hub
-docker push livechange/db-server-aarch64:$VERSION
-docker push livechange/db-server-aarch64:latest
-
--- db-server/build-docker-and-upload.sh
+++ db-server/build-docker-and-upload.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 VERSION=`echo "console.log(require('./package.json').version)" | node`
+PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7"
 echo building docker image for version $VERSION
-docker build -t livechange/db-server:$VERSION -t livechange/db-server:latest .
-echo uploading docker image to docker hub
-docker push livechange/db-server:$VERSION
-docker push livechange/db-server:latest
+docker buildx build --platform $PLATFORMS -t livechange/db-server:$VERSION -t livechange/db-server:latest --push .
 
