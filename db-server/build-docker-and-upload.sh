3a8850cf4b44da20de1670fbb5220af7d9e834fd
Michał Łaszczewski <michal@laszczewski.com>
2022-12-15 10:36:55 +0100

    db admin vue update

--- db-server/build-docker-and-upload.sh
+++ db-server/build-docker-and-upload.sh
@@ -3,4 +3,3 @@ VERSION=`echo "console.log(require('./package.json').version)" | node`
 PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7"
 echo building docker image for version $VERSION
 docker buildx build --platform $PLATFORMS -t livechange/db-server:$VERSION -t livechange/db-server:latest --push .
-
