8b598b21961e94c29c7c78434ca68d2366aa005d
Michał Łaszczewski <michal@laszczewski.com>
2024-10-01 10:06:12 +0200

    i18n fix

--- frontend/frontend-template/front/locales/en.js
+++ frontend/frontend-template/front/locales/en.js
@@ -1,4 +1,30 @@
-import messages from "./en.json"
+import messagesJson from "./en.json"
+
+import { locales as autoFormLocales } from "@live-change/frontend-auto-form"
+
+function mergeDeep(...sources) {
+  let acc = {}
+  for (const source of sources) {
+    if (source instanceof Array) {
+      if (!(acc instanceof Array)) {
+        acc = []
+      }
+      acc = acc.concat(source)
+    } else if (source instanceof Object) {
+      for (const [key, value] of Object.entries(source)) {
+        if (value instanceof Object && key in acc) {
+          acc[key] = mergeDeep(acc[key], value)
+        } else {
+          acc[key] = value
+        }
+      }
+    }
+  }
+  return acc
+}
+
+const messages = mergeDeep(autoFormLocales.en, messagesJson)
+
 export { messages }
 
 export const numberFormats ={
