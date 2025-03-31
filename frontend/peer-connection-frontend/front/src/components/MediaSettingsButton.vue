fc1943944b283817c22cc87c7926525fe970b3ae
Michał Łaszczewski <michal@laszczewski.com>
2025-03-31 20:40:04 +0200

    MediaSettingsButton bug fix

--- frontend/peer-connection-frontend/front/src/components/MediaSettingsButton.vue
+++ frontend/peer-connection-frontend/front/src/components/MediaSettingsButton.vue
@@ -2,62 +2,77 @@
   <div>
     <OverlayPanel ref="mediaSettingsOverlay">
       <div class="flex flex-col gap-2 pt-2 justify-around" style="min-width: 20rem; max-width: 90vw">
+
         <div v-if="audioInputRequest !== 'none' && model.audioInputs?.length > 0"
              class="flex flex-col items-stretch grow">
           <div class="text-sm mb-1 pl-1">Microphone</div>
-          <Dropdown :modelValue="model.audioInput"
-                    @update:modelValue="value => updateAudioInput(value)"
-                    :options="model.audioInputs"
-                    optionLabel="label"
-                    placeholder="Select">
+
+          <Select :modelValue="model.audioInput.deviceId"
+                  @update:modelValue="value => updateAudioInput(value)"
+                  :options="model.audioInputs"
+                  optionLabel="label"
+                  optionValue="deviceId"
+                  placeholder="Select">
             <template #value="slotProps">
               <div class="flex flex-row items-center">
                 <i class="pi pi-microphone mr-2" />
                 &nbsp;
                 <div class="absolute overflow-hidden text-ellipsis" style="left: 2em; right: 2em;">
-                  {{ slotProps.value ? slotProps.value.label : slotProps.placeholder }}
+                  {{ slotProps.value
+                    ? model.audioInputs.find(ai => ai.deviceId === slotProps.value)?.label
+                    : slotProps.placeholder }}
                 </div>
               </div>
             </template>
-          </Dropdown>
+          </Select>
         </div>
+
         <div v-if="audioOutputRequest !== 'none' && model.audioOutputs?.length > 0"
              class="flex flex-col items-stretch grow">
           <div class="text-sm mb-1 pl-1">Audio output</div>
-          <Dropdown :modelValue="model.audioOutput"
+          <Select :modelValue="model.audioOutput.deviceId"
                     @update:modelValue="value => updateAudioOutput(value)"
-                    :options="model.audioOutputs" optionLabel="label"
+                    :options="model.audioOutputs"
+                    optionLabel="label"
+                    optionValue="deviceId"
                     placeholder="Select">
             <template #value="slotProps">
               <div class="flex flex-row items-center">
                 <i class="pi pi-volume-up mr-2" />
                 &nbsp;
                 <div class="absolute overflow-hidden text-ellipsis" style="left: 2em; right: 2em;">
-                  {{ slotProps.value ? slotProps.value.label : slotProps.placeholder }}
+                    {{ slotProps.value
+                      ? model.audioOutputs.find(ao => ao.deviceId === slotProps.value)?.label
+                      : slotProps.placeholder }}
                 </div>
               </div>
             </template>
-          </Dropdown>
+          </Select>
         </div>
 
         <div v-if="videoInputRequest !== 'none' && model.videoInputs?.length > 0"
              class="flex flex-col items-stretch grow">
           <div class="text-sm mb-1 pl-1">Camera</div>
-          <Dropdown :modelValue="model.videoInput"
+          <Select :modelValue="model.videoInput.deviceId"
                     @update:modelValue="value => updateVideoInput(value)"
-                    :options="model.videoInputs" optionLabel="label"
+                    :options="model.videoInputs"
+                    optionLabel="label"
+                    optionValue="deviceId"
                     placeholder="Select">
             <template #value="slotProps">
               <div class="flex flex-row items-center">
                 <i class="pi pi-camera mr-2" />
                 &nbsp;
                 <div class="absolute overflow-hidden text-ellipsis" style="left: 2em; right: 2em;">
-                  {{ slotProps.value ? slotProps.value.label : slotProps.placeholder }}
+                  {{ slotProps.value
+                    ? model.videoInputs.find(vi => vi.deviceId === slotProps.value)?.label
+                    : slotProps.placeholder }}
                 </div>
               </div>
             </template>
-          </Dropdown>
+          </Select>
         </div>
+
       </div>
     </OverlayPanel>
 
@@ -69,7 +84,7 @@
 <script setup>
   import Button from 'primevue/button'
   import OverlayPanel from 'primevue/overlaypanel'
-  import Dropdown from 'primevue/dropdown'
+  import Select from 'primevue/select'
 
 
   import { defineModel, defineProps, defineEmits, computed, ref, toRefs, onMounted } from 'vue'
@@ -129,19 +144,19 @@
   function updateAudioInput(value) {
     model.value = {
       ...model.value,
-      audioInput: value
+      audioInput: model.value.audioInputs.find(ai => ai.deviceId === value)
     }
   }
   function updateAudioOutput(value) {
     model.value = {
       ...model.value,
-      audioOutput: value
+      audioOutput: model.value.audioOutputs.find(ao => ao.deviceId === value)
     }
   }
   function updateVideoInput(value) {
     model.value = {
       ...model.value,
-      videoInput: value
+      videoInput: model.value.videoInputs.find(vi => vi.deviceId === value)
     }
   }
 
