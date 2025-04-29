d31b69e77310268e84bf6a04cb9299354a4d67b2
Michał Łaszczewski <michal@laszczewski.com>
2025-04-29 08:15:56 +0200

    dao request error logging

--- dao/dao/lib/ReactiveConnection.js
+++ dao/dao/lib/ReactiveConnection.js
@@ -148,6 +148,7 @@ class Connection extends EventEmitter {
           return reject(err)
         }
         if (resp.type === 'error') {
+          console.error("ERROR", resp.error, "ON REQUEST", msg, "WITH SETTINGS", settings)
           reject(resp.error)
           return false
         }
