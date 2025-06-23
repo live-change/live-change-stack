754465b9f36edc2804f1a8c696698b48b3b4ef5e
Michał Łaszczewski <michal@laszczewski.com>
2025-06-23 12:26:17 +0200

    database docker fix

--- db/db-server/build-docker-and-upload.sh
+++ db/db-server/build-docker-and-upload.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 VERSION=`echo "console.log(require('./package.json').version)" | node`
 PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7"
+export BUILDKIT_PROGRESS=plain
+export DOCKER_BUILDKIT=1 
 #PLATFORMS="linux/amd64"
 echo building docker image for version $VERSION
-docker buildx use hybrid-builde
+docker buildx use hybrid-builder
 docker buildx build --debug \
+  --builder hybrid-builder \
+  --build-arg VERSION=$VERSION \
   --platform $PLATFORMS \
   -t livechange/db-server:$VERSION \
   -t livechange/db-server:latest \
